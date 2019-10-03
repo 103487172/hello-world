ad29ce6378914ec3937c40303864dba943f2f8a8
nhatminh200 <55097525+nhatminh200@users.noreply.github.com>
2019-10-03 16:37:16 +0700

    tạm thời mới tới vậy thôi anh

--- app.kt
+++ app.kt
@@ -1,4 +1,47 @@
 fun main() {
-    println("Hello, World!")
 
+        println("x = $x; PI = $PI")
+        incrementX()
+        println("incrementX()")
+        println("x = $x; PI = $PI")
+
+        val result = maxOf(10, 6)
+        println(result)
+
+    val sum = 10 + 9
+    println(sum)
+
+    println("Phan Nhat Minh")
+
+    println(a1)
+
+    val items = listOf("apple", "banana", "kiwifruit")
+    for (item in items) {
+        println(item)}
+
+    var index = 0
+    while (index < items.size) {
+        println("item at $index is ${items[index]}")
+        index++
+    }
 }
+
+val PI = 3.14
+
+var x = 0
+fun incrementX() {
+    x += 1
+}
+
+
+fun maxOf(a: Int, b: Int): Int {
+    if (a > b) {
+        return a
+    } else {
+        return b
+    }
+}
+
+var a=1
+val a1 = "to be continue"
+
